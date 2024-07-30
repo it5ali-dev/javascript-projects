737068652a06becc3bacaaf15c7663f6156b8215
Ali Hassan <160418871+it5ali-dev@users.noreply.github.com>
2024-07-30 13:56:02 +0500

    project-3 (rotating navigation menu)

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,2 +1,19 @@
 'use strict'
 
+let home = document.querySelector(".home")
+let bars = document.querySelector('.bars')
+let cancel = document.querySelector('.cancel')
+let menu = document.querySelector('.menu')
+
+bars.addEventListener("click", function () {
+    home.classList.toggle('rotate')
+    // menu.classList.toggle('rotate')
+    bars.classList.toggle('hide')
+    cancel.classList.toggle('hide')
+})
+cancel.addEventListener("click", function () {
+    home.classList.toggle('rotate')
+    // menu.classList.toggle('rotate')
+    bars.classList.toggle('hide')
+    cancel.classList.toggle('hide')
+})
